6182fa7b8df73971a006cb9aa755ceabf3587eb5
drabiot <tchartie@student.42angouleme.fr>
2026-05-20 16:16:39 +0200

    Add undefinned behaviour test for trace with not squarred matrix

--- ressources/ex08Tester.cpp
+++ ressources/ex08Tester.cpp
@@ -6,7 +6,7 @@
 /*   By: tchartie <tchartie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/20 15:47:09 by tchartie          #+#    #+#             */
-/*   Updated: 2026/05/20 15:52:01 by tchartie         ###   ########.fr       */
+/*   Updated: 2026/05/20 16:14:05 by tchartie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	traceTester() {
 	Matrix	<double>m1({{1, 0}, {0, 1}});
 	Matrix	<double>m2({{2, -5, 0}, {4, 3, 7}, {-2, 3, 4}});
 	Matrix	<double>m3({{-2, -8, 4}, {1, -23, 4}, {0, 6, 4}});
+	Matrix	<double>m4({{-2, -8, 4}, {1, -23, 4}});
+	Matrix	<double>m5({{-2, -8}, {1, -23}, {0, 6}});
 
 	PRINT MAGENTA "Fist Matrix:" CENDL;
 	PRINT m1;
@@ -31,5 +33,15 @@ void	traceTester() {
 	PRINT m3;
 	PRINT BLUE "Trace" CENDL;
 	PRINT CYAN AND m3.trace() CENDL;
+	NLINE;
+	PRINT MAGENTA "Fourth Matrix:" CENDL;
+	PRINT m4;
+	PRINT BLUE "Trace" CENDL;
+	PRINT CYAN AND m4.trace() CENDL;
+	NLINE;
+	PRINT MAGENTA "Fifth Matrix:" CENDL;
+	PRINT m5;
+	PRINT BLUE "Trace" CENDL;
+	PRINT CYAN AND m5.trace() CENDL;
 	NLINE;
 }
