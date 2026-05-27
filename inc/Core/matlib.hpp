8051d949ae4c69b4160e68f4667568cc8b7aedb7
drabiot <tchartie@student.42angouleme.fr>
2026-05-27 15:48:18 +0200

    Respect NDC in projection matrix

--- inc/Core/matlib.hpp
+++ inc/Core/matlib.hpp
@@ -6,7 +6,7 @@
 /*   By: tchartie <tchartie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/15 16:51:03 by tchartie          #+#    #+#             */
-/*   Updated: 2026/05/27 14:42:54 by tchartie         ###   ########.fr       */
+/*   Updated: 2026/05/27 15:47:20 by tchartie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,18 +21,18 @@
 # include "../../src/Functions/Cosine.tpp"
 # include "../../src/Functions/Cross_Product.tpp"
 
-inline Matrix<float>	projection(float fov, float ratio, float near, float far) {
+inline Matrix<float> projection(float fov, float ratio, float near, float far) {
 	Matrix<float>	ret(4);
 	float			scale;
 
-	scale = 1.0f / tan(fov * 0.5f * M_PI / 180.0f);
+	scale = 1.0f / tan(fmaf(fov, 0.5f * M_PI / 180.0f, 0.0f));
 
 	ret[0][0] = scale / ratio;
 	ret[1][1] = scale;
 	ret[2][2] = -far / (far - near);
-	ret[3][2] = -far * near / (far - near);
 	ret[2][3] = -1.0f;
-	ret[3][3] = 0.0;
+	ret[3][2] = -(far * near) / (far - near);
+	ret[3][3] = 0.0f;
 
 	return (ret);
 }
